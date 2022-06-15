1f50989f5f6cf1f34801d4e7e4a735ac48b1dfb1
RZhang <richardzhiyuanzhang@gmail.com>
2022-06-15 17:24:26 -0400

    added a GET * return

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
