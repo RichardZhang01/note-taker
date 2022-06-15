04c0eda7e38fb227ba87febd1f45bb801568de8b
RZhang <richardzhiyuanzhang@gmail.com>
2022-06-15 13:40:37 -0400

    refactored files

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,35 @@
-const notes = require('express').Router();
-const path = require('path');
+const api = require('express').Router();
+const { readFromFile, readAndAppend, readFilterAndAppend } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
 
-notes.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+api.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// notes.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/index.html'));
-// });
+api.post('/', (req, res) => {
+    const { title, text } = req.body;
+    if (req.body) {
+        const newNote = {
+        title,
+        text,
+        id: uuidv4(),
+        };
 
-module.exports = notes;
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
+api.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    if (id) {
+        readFilterAndAppend('./db/db.json', id);
+        res.json(`Note deleted successfully`);
+    } else {
+        res.error('Error in deleting note');
+    }
+});
+
+module.exports = api;
